1530ff7ebb6238efa0b31e72c568968d7af1935e
Joandra2924 <putrij0916@gmail.com>
2026-07-06 07:52:57 +0800

    update

---
+++ labs/lab03/exercise2/lab-03-2.py
@@ -0,0 +1,3 @@
+numNight = int(input())
+totalPayment = numNight * 250 * 1.15
+print(totalPayment)
---
+++ labs/lab03/exercise3/lab-03-3.py
@@ -0,0 +1,9 @@
+kilobyte = int(input())
+megabyte = megabyte / 1024
+gigabyte = gigabyte / 1024
+terabyte = terabyte / 1024
+petabyte = petabyte / 1024
+print(megabyte)
+print(gigabyte)
+print(terabyte)
+print(petabyte)
